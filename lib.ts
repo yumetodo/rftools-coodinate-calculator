ed75db495b40c4274e9a649e88753e44c5e30e39
yumetodo <yume-wikijp@live.jp>
2021-06-20 02:07:17 +0900

    add impl
    ref:
    https://github.com/McJtyMods/RFTools/blob/0a2e1e4108926840054742a0a7f1c2070857fac1/src/main/java/mcjty/rftools/items/builder/ShapeCardItem.java#L117-L119

--- lib.ts
+++ lib.ts
@@ -4,4 +4,9 @@ export interface RFToolsCoordinate {
   offset: Point;
 }
 export function calc(builderPos: Point, p1: Point, p2: Point): RFToolsCoordinate {
+  const center = [Math.ceil((p1[0] + p2[0]) / 2), Math.ceil((p1[1] + p2[1]) / 2), Math.ceil((p1[2] + p2[2]) / 2)];
+  return {
+    dim: [Math.abs(p1[0] - p2[0]) + 1, Math.abs(p1[1] - p2[1]) + 1, Math.abs(p1[2] - p2[2]) + 1],
+    offset: [center[0] - builderPos[0], center[1] - builderPos[1], center[2] - builderPos[2]],
+  };
 }
